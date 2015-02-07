a97c42c6b7b4090fda6465ada9e9e3495edd6fe0
Akhilesh <Pheli@Nepheli-5.local>
2015-02-07 14:45:18 -0800

    Initial Commit

---
+++ DBPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DBPractice
+//
+//  Created by Akhilesh on 2/7/15.
+//  Copyright (c) 2015 Akhilesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DBPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DBPractice
+//
+//  Created by Akhilesh on 2/7/15.
+//  Copyright (c) 2015 Akhilesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
